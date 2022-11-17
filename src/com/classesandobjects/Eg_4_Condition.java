a8ea36b7e4adc0eeafbf64afaefd5b0ac0751261
tejashrikalbhor <tejashri.kalbhor@siliconstack.com.au>
2022-11-17 17:02:59 +0530

    validation on age and percentage

--- src/com/classesandobjects/Eg_4_Condition.java
+++ src/com/classesandobjects/Eg_4_Condition.java
@@ -13,11 +13,13 @@ class Student{
         System.out.println("Create a new Record");
         System.out.println("----------------------------");
         System.out.println("Enter the name:");
-        String name = sc.next();
+        name = sc.next();
         System.out.println("Enter the age:");
-        int age = sc.nextInt();
+         age = sc.nextInt();
+        validAge();
         System.out.println("Enter the percentage:");
-        float per =sc.nextFloat();
+         percentage =sc.nextFloat();
+        validPercentage();
         System.out.println("----------------------------");
         System.out.println("New Record Added Successfully..");
         System.out.println("----------------------------");
@@ -27,12 +29,30 @@ class Student{
         System.out.println("Age is : "+age);
         System.out.println("Percentage is : "+percentage);
     }
+    void validAge(){
+        int count =0;
+        if(age<3){
+            count = 3 - age ;
+            System.out.println("Age criteria does not match.try again after " + count+" years.");
+            }
+        else if (age<=16){
+
+        }
+        else{
+            System.out.println("Age criteria is eligible for higher studies.Good luck!");
+        }
+        }
+        void validPercentage() {
+        if(percentage <=0 ||percentage >100){
+            System.out.println("Invalid percentage.Percentage range should be between 1 to 100.");
+        }
+        }
 }
+
 public class Eg_4_Condition {
     public static void main(String[] args) {
         Student s =new Student();
         s.addRecord();
         s.showDetails();
-
     }
 }
