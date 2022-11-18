c72d7254424b50d35f9119cdac9dac052a78f687
tejashrikalbhor <tejashri.kalbhor@siliconstack.com.au>
2022-11-18 17:47:23 +0530

    non-static variable and method

--- src/com/classesandobjects/Eg_6_Non_StaticMethod_Variable.java
+++ src/com/classesandobjects/Eg_6_Non_StaticMethod_Variable.java
@@ -4,14 +4,24 @@ public class Eg_6_Non_StaticMethod_Variable {
     // non-static variable and method
 
      String  name = "tejashri";
-     void show(){
+     int age =23;
+      String country ="India";
+    {
+        System.out.println("you are in the non-static block..");
+        show();
+    }
+
+      void show(){
          System.out.println("Name is : "+name);
+         System.out.println("Age is : "+age);
+         System.out.println("Country is : "+country);
      }
 
     public static void main(String[] args) {
          Eg_6_Non_StaticMethod_Variable s =new Eg_6_Non_StaticMethod_Variable();
+       s.show();
 
-        s.show();
+
     }
 
 }
