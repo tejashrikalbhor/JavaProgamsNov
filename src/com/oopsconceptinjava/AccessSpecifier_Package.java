db9e1495cfd78037b3b9e1066b420d9e7f5b869d
tejashrikalbhor <tejashri.kalbhor@siliconstack.com.au>
2022-11-24 15:55:24 +0530

    we can access public,default,protected variables is same package except private

---
+++ src/com/oopsconceptinjava/AccessSpecifier_Package.java
@@ -0,0 +1,26 @@
+package com.oopsconceptinjava;
+
+class C{
+    int a=10;
+    public int b=20;
+    private int c=30;
+    protected int d=40;
+    void showDetails(){
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
+public class AccessSpecifier_Package {
+    public static void main(String[] args) {
+        C cc = new C();
+        cc.showDetails();
+        System.out.println(cc.a);
+        System.out.println(cc.b);
+//        System.out.println(cc.c);
+        System.out.println(cc.d);
+
+    }
+
+}
