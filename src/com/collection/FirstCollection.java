23512dae26f321be18a822061b2e5f2dc9442e49
tejashrikalbhor <tejashri.kalbhor@siliconstack.com.au>
2022-11-28 12:49:51 +0530

    my first collection eg

--- src/com/collection/FirstCollection.java
+++ src/com/collection/FirstCollection.java
@@ -1,8 +1,17 @@
 package com.collection;
 
+import java.util.ArrayList;
+
 public class FirstCollection {
     public static void main(String[] args) {
         System.out.println("welcome to my first collection..");
+        ArrayList<String> names= new  ArrayList<>();
+        names.add("tejashri");
+        names.add("rajeshri");
+        names.add("john");
+        names.add("sohaa");
+        System.out.println("this is my array list..");
+        System.out.println(names);
 
     }
 }
