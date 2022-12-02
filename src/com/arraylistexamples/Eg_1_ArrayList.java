04416504ef38aff0c22208eafa27bbc7024e9a9d
tejashrikalbhor <tejashri.kalbhor@siliconstack.com.au>
2022-12-02 16:37:05 +0530

    Array List parameter constructor

--- src/com/arraylistexamples/Eg_1_ArrayList.java
+++ src/com/arraylistexamples/Eg_1_ArrayList.java
@@ -13,7 +13,23 @@ public class Eg_1_ArrayList {
         name.add("Employee");
         name.add("Silicon stack");
         System.out.println("you are in the main method");
+        System.out.println("Array constructor");
+        System.out.println("1.ArrayList()");
         System.out.println(name);
+
+        // 2.ArrayList(size);
+        List<Integer> price = new ArrayList<>(5);
+        System.out.println("2.ArrayList(5)with size ");
+        price.add(10);
+        price.add(20);
+        price.add(30);
+        price.add(40);
+        price.add(50);
+        price.add(60);
+        System.out.println("size of array list is :"+price.size());
+        System.out.println(price);
+
+
 
     }
 }
