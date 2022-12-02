778a19bba5dae450f4d583497d44878b163f3505
tejashrikalbhor <tejashri.kalbhor@siliconstack.com.au>
2022-12-02 16:26:44 +0530

    Array List constructor

---
+++ src/com/arraylistexamples/Eg_1_ArrayList.java
@@ -0,0 +1,19 @@
+package com.arraylistexamples;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Eg_1_ArrayList {
+    public static void main(String[] args) {
+        // there are 3 constructors used for ArrayList
+        // 1.ArrayList();
+        List<String> name =new ArrayList<>();
+        name.add("tejashri");
+        name.add("kalbhor");
+        name.add("Employee");
+        name.add("Silicon stack");
+        System.out.println("you are in the main method");
+        System.out.println(name);
+
+    }
+}
