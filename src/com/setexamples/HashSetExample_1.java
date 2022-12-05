062df2cfb2c58bd7d179a0221858dd6b04a9b845
tejashrikalbhor <tejashri.kalbhor@siliconstack.com.au>
2022-12-05 11:47:09 +0530

    hashset eg

--- src/com/setexamples/HashSetExample_1.java
+++ src/com/setexamples/HashSetExample_1.java
@@ -7,11 +7,16 @@ public class HashSetExample_1 {
     public static void main(String[] args) {
         System.out.println("you are in the main..");
         Set<Integer>s =new HashSet<>();
+        s.add(1);
+        s.add(2);
+        s.add(3);
+        s.add(4);
+        s.add(5);
         s.add(10);
         s.add(20);
         s.add(30);
-        s.add(20);
-        s.add(10);
+        s.add(40);
+        s.add(50);
         boolean show = s.contains(10);
         System.out.println("you are in the set of integers..");
         System.out.println(s);
