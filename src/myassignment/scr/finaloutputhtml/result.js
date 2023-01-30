9b833a62197b47828afa2b28e4ba97ee5f89ed9a
tejashrikalbhor <tejashri.kalbhor@siliconstack.com.au>
2023-01-30 10:20:50 +0530

    count

--- src/myassignment/scr/finaloutputhtml/result.js
+++ src/myassignment/scr/finaloutputhtml/result.js
@@ -3,14 +3,21 @@ debugger
 
 
 var storedArray = JSON.parse(sessionStorage.getItem("items"));
-//var arrPass=new Array();
-//var arrFail=new Array();
-//countStatus();
-//document.getElementById("countPass").innerHTML=arrPass;
-//document.getElementById("countFail").innerHTML=arrFail;
+var duplicateArray = storedArray;
+
+var countPass =duplicateArray==null?0:duplicateArray.filter(function(element){return (element.status == 'pass')||(element.status == 'Pass')||(element.status == 'PASS');}).length
+
+var countFail =duplicateArray==null?0:duplicateArray.filter(function(element){return (element.status == 'fail')||(element.status == 'Fail')||(element.status == 'FAIL');}).length
+
+
+document.getElementById("countPass").innerHTML=countPass;
+document.getElementById("countFail").innerHTML=countFail;
 
 var totalCount=storedArray==null?0:storedArray.length;
 document.getElementById("steps").innerHTML=totalCount;
+
+
+
 
 display_current_date_hopePage();
 
@@ -18,15 +25,24 @@ for (let i = 0; i < storedArray.length; i++) {
 	var name = storedArray[i].name;
 	var status = storedArray[i].status;
 	var current_date = storedArray[i].current_date;
-    var html=`<ul class='collapsible node-list' data-collapsible='accordion'><li class='node level-1 leaf pass' status='pass' test-id='2'><div class='collapsible-header'><div class='node-name'>${name}</div> <span class='node-time'>${current_date}</span>&middot; <span class='node-duration'>0h 2m 32s+291ms</span><span class='test-status right pass'> ${status}</span></div> `;
+if((status == 'pass')||(status == 'Pass')||(status == 'PASS')){
+    var html=`<ul class='collapsible node-list' data-collapsible='accordion'><li class='node level-1 leaf pass' status='pass' id='pass'><div class='collapsible-header'><div class='node-name'>${name}</div> <span class='node-time'>${current_date}</span>&middot; <span class='node-duration'>0h 2m 32s+291ms</span><span class='test-status right pass'> ${status}</span></div><div class='collapsible-body'></div> `;
+    document.getElementById("tbl").insertAdjacentHTML("beforeend",html);
+}
+else{
+if((status == 'fail')||(status == 'Fail')||(status == 'FAIL'))
+	{
+    var html=`<ul class='collapsible node-list' data-collapsible='accordion'><li class='node level-1 leaf fail' status='fail' id='fail'><div class='collapsible-header'><div class='node-name'>${name}</div> <span class='node-time'>${current_date}</span>&middot; <span class='node-duration'>0h 2m 32s+291ms</span><span class='test-status right fail'> ${status}</span></div><div class='collapsible-body'></div> `;
 	document.getElementById("tbl").insertAdjacentHTML("beforeend",html);
+
+}
+}
+
 }
 })
 
-
-
-
 function current_Date(){
+
 var options = {  year: 'numeric', month: 'short', day: 'numeric' };
 var today  = new Date();
 
@@ -35,9 +51,9 @@ var curr_time=today.toLocaleString("en-US");
 let curr_time_am_pm=curr_time.slice(10);
 
 var current_date_time =curr_date+curr_time_am_pm;
-
 return current_date_time;
 }
+
 function display_current_date_hopePage(){
 
 var display_date=current_Date();
@@ -50,22 +66,6 @@ document.getElementById("end-time").innerHTML=display_date;
 document.getElementById("panel-lead-start-time").innerHTML=display_date;
 document.getElementById("panel-lead-end-time").innerHTML=display_date;
 
+}
 
-}
-function countStatus(){
-var countPass=0;
-var countFail=0;
-for (let i = 0; i < storedArray.length; i++) {
-var status=storedArray[i].status;
-if(status =="pass"||status =="Pass"||status =="PASS"){
-countPass=countPass+1;
- arrPass.push(countPass);
-}
-else{
-if(status =="fail"||status =="Fail"||status =="FAIL"){
-countFail=countFail +1;
- arrFail.push(countFail);
-}
-}
-}
-}
+
